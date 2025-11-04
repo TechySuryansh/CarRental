8bf58b54935df0f786fff0d0966c929766037464
SuryanshSingh7 <singhs@alphavima.com>
2025-11-04 23:55:52 +0530

    first commit

---
+++ client/src/components/CarCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CarCard = ({car}) => {
+  return (
+    <div className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
+       
+       <div >
+        <img src={car.image} alt="Car Image" className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-105'/>
+        {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full'>Available Now</p>}
+        <div className='absolute bottom-4 right-4 bg-black/80 blackdrop-blur-sm text-white px-3 py-2 rounded-lg'>
+
+        </div>
+       </div>
+        </div>
+  )
+}
+
+export default CarCard
---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDB from "./configs/db.js"
+import userRouter from "./routes/userRoutes.js"
+
+const app=express() //initialise express app
+await connectDB()  //connect database
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>res.send("Server is Running"))
+app.use('/api/user',userRouter)
+
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
